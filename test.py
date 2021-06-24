7b3e07dafebd5a49a790215ae9a606e5f41e0600
dheera <dheera@dheera.net>
2021-06-24 21:20:35 +0000

    test node

---
+++ test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+try:
+    import rospy
+except ImportError:
+    import rospy2 as rospy
+
+from std_msgs.msg import String
+
+class TestROS1Node(object):
+    def __init__(self):
+        rospy.init_node("test_node")
+        rospy.logdebug("initializing")
+        self.sub_message = rospy.Subscriber("/message", String, self.on_message)
+        self.pub_message = rospy.Publisher("/message", String, queue_size = 1)
+        self.count = 0
+        rospy.loginfo_once("this is a info message that is supposed to only appear once")
+        rospy.loginfo_once("this is a info message that is supposed to only appear once")
+
+    def start(self):
+        self.rate = rospy.Rate(10)
+
+        while not rospy.is_shutdown():
+            self.rate.sleep()
+            self.count += 1
+            self.pub_message.publish("foo %d" % self.count)
+            rospy.logwarn_throttle(2, "this is a warning message that should appear at most every 2 seconds")
+
+        rospy.logwarn("shutting down")
+
+    def on_message(self, msg):
+        rospy.loginfo("received message: %s", msg.data)
+
+if __name__ == "__main__":
+    TestROS1Node().start()
+
